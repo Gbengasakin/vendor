14d5b275a3daa7a4b0d282420bf2a5de76b73378
Olonisakin Gbenga <96980050+Gbengasakin@users.noreply.github.com>
2022-09-26 16:02:23 +0100

    Add files via upload

---
+++ vendor/js/app.js
@@ -0,0 +1,12 @@
+function ope(){
+	document.getElementById('menu').style.display = 'block';
+	document.getElementById('bar').style.display = 'none';
+	document.getElementById('close').style.display = 'block';
+
+}
+function clos(){
+	document.getElementById('menu').style.display = 'none';
+		document.getElementById('bar').style.display = 'block';
+			document.getElementById('close').style.display = 'none';
+
+}
